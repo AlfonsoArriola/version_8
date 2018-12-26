ee7e84f224547aee1c895e6399ef92743afa972e
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-12-26 00:46:22 -0500

    ugh...waiting for js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// ++++++++++++   Loader  ++++++++++++++++++
 window.addEventListener('load', function (){
 	const loader = document.querySelector(".loader");
 	loader.className += ' hidden';
 });
 
-
+// +++++++++++++   Nav Bar  ++++++++++++++++++++++++
 
 function openNav() {
     document.getElementById("mySidenav").setAttribute("style", "width:250px;box-shadow:1px 1px 1px 1px #eae3e3;");
@@ -13,7 +14,7 @@ function closeNav() {
     document.getElementById("mySidenav").setAttribute("style", "width:0px;box-shadow:initial;");
 }
 
-
+// ++++++      Banner Resizing   ++++++++++++++++
 window.onscroll = function() {slimScrollGo()};
 
 function slimScrollGo() {
